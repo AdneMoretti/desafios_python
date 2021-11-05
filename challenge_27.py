ceab040d062dd2fb95b9e44249ac4d653e10778e
AdneMoretti <morettiadne1@gmail.com>
2021-11-05 17:33:15 -0300

    adicionando desafio de positivos e media

---
+++ challenge_27.py
@@ -0,0 +1,9 @@
+positivo = 0
+soma = 0
+for i in range(6):
+    numero = float(input())
+    if numero>0:
+       positivo+=1
+       soma+=numero
+print(positivo, "valores positivos")
+print("{:.1f}".format(soma/positivo))
